4ed7a82aa1e7e2ff913bf24e81e7c9c9f103be18
Samille Machado <sami.bmachado@gmail.com>
2022-06-14 23:43:44 -0300

    firs commit

---
+++ public/js/index.js
@@ -0,0 +1,196 @@
+"use strict";
+//LOGIN
+let formLogin = document.querySelector("#form-login");
+let inputEmailLogin = document.querySelector("#input-email");
+let inputSenhaLogin = document.querySelector("#input-senha");
+let btnCriar = document.querySelector("#btn-criar");
+let btnVoltarLogin = document.querySelector("#btn-voltar-login");
+let divPrincipalLogin = document.querySelector("#div-principal-login");
+let divPrincipalSignup = document.querySelector("#div-principal-signup");
+let usuarios = JSON.parse(localStorage.getItem("usuarios") || "[]");
+// CADASTRO
+let formSignup = document.querySelector("#form-signup");
+let labelInputEmailSignup = document.querySelector("#label-input-email");
+let inputEmailSignup = document.querySelector("#input-signup-email");
+let validEmailSignup = false;
+let labelInputSenhaSignup = document.querySelector("#label-input-senha");
+let inputSenhaSignup = document.querySelector("#input-signup-senha");
+let validSenhaSignup = false;
+let labelInputRepeteSenhaSignup = document.querySelector("#label-input-repete-senha");
+let inputRepeteSenhaSignup = document.querySelector("#input-signup-repete-senha");
+let validRepeteSenhaSignup = false;
+let regSenha = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+// ALERTAS
+let espacoAlertaIndex = document.getElementById('espaco-alerta-index');
+let corpoAlertaIndex = document.createElement('div');
+function mostrarAlertaIndex(mensagem, tipo) {
+    corpoAlertaIndex.innerHTML = `
+                        <div class="alert alert-${tipo} alert-dismissible fade show" role="alert">
+                                <div>${mensagem}</div>
+                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                        `;
+    espacoAlertaIndex.appendChild(corpoAlertaIndex);
+    setTimeout(() => { corpoAlertaIndex.innerHTML = ''; }, 3000);
+}
+;
+// EVENTOS
+formLogin.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (verificaCamposLogin()) {
+        if (validaUsuarioLogin() && validaSenhaLogin()) {
+            salvarNoLocalStorage();
+            redirecionaParaPaginaPainel();
+        }
+    }
+});
+btnCriar.addEventListener("click", () => {
+    formLoginHide();
+    formSignupShow();
+});
+btnVoltarLogin.addEventListener("click", () => {
+    formSignupHide();
+    formLoginShow();
+});
+inputEmailSignup.addEventListener("keyup", verificaEmailSignup);
+inputSenhaSignup.addEventListener("keyup", verificaSenhaSignup);
+inputRepeteSenhaSignup.addEventListener("keyup", verificaRepeteSenhaSignup);
+formSignup.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (verificaCampos()) {
+        criarRegistroUsuario();
+        limparCampos();
+    }
+});
+// FUNÇÕES
+function verificaCamposLogin() {
+    let retorno = true;
+    if (inputEmailLogin.value === "" || inputSenhaLogin.value === "") {
+        mostrarAlertaIndex("Você deve preencher todos os campos!", "warning");
+        retorno = false;
+    }
+    return retorno;
+}
+function validaUsuarioLogin() {
+    let usuarioExistente = usuarios.some((usuario) => usuario.email === inputEmailLogin.value);
+    if (!usuarioExistente) {
+        mostrarAlertaIndex("Usuário não cadastrado! Primeiro crie uma conta.", "warning");
+    }
+    return usuarioExistente;
+}
+function validaSenhaLogin() {
+    let usuario = usuarios.find((usuario) => usuario.email === inputEmailLogin.value);
+    let senhaCorreta = usuario ? usuario.senha === inputSenhaLogin.value : false;
+    if (!senhaCorreta) {
+        mostrarAlertaIndex("Senha incorreta, tente novamente.", "danger");
+    }
+    return senhaCorreta;
+}
+function redirecionaParaPaginaPainel() {
+    alert("Você logou!");
+    window.location.href = "../public/painel.html";
+}
+function salvarNoLocalStorage() {
+    localStorage.setItem("usuario_logado", inputEmailLogin.value);
+}
+function formLoginHide() {
+    divPrincipalLogin.classList.add("d-none");
+}
+function formSignupShow() {
+    divPrincipalSignup.classList.remove("d-none");
+}
+function formSignupHide() {
+    divPrincipalSignup.classList.add("d-none");
+}
+function formLoginShow() {
+    divPrincipalLogin.classList.remove("d-none");
+}
+function verificaEmailSignup() {
+    if (inputEmailSignup.value.length < 10) {
+        labelInputEmailSignup.style.color = 'red';
+        labelInputEmailSignup.innerHTML = "e-mail: * insira no mínimo 10 caracteres";
+        validEmailSignup = false;
+    }
+    else {
+        labelInputEmailSignup.style.color = '#592c12';
+        labelInputEmailSignup.innerHTML = "e-mail:";
+        validEmailSignup = true;
+    }
+}
+;
+function verificaSenhaSignup() {
+    let senhaValida = inputSenhaSignup.value.match(regSenha);
+    if (inputSenhaSignup.value.length < 6) {
+        labelInputSenhaSignup.style.color = 'red';
+        labelInputSenhaSignup.innerHTML = "senha: * insira no mínimo 6 caracteres";
+        validSenhaSignup = false;
+    }
+    else if (senhaValida === null) {
+        labelInputSenhaSignup.innerHTML = "senha: * senha deve conter uma letra maiúscula e um caracter especial";
+        labelInputSenhaSignup.style.color = 'red';
+        validSenhaSignup = false;
+    }
+    else {
+        labelInputSenhaSignup.style.color = '#592c12';
+        labelInputSenhaSignup.innerHTML = "senha:";
+        validSenhaSignup = true;
+    }
+}
+;
+function verificaRepeteSenhaSignup() {
+    if (inputRepeteSenhaSignup.value !== inputSenhaSignup.value) {
+        labelInputRepeteSenhaSignup.style.color = 'red';
+        labelInputRepeteSenhaSignup.innerHTML = "repete senha: * a senha digitada não corresponde";
+        validRepeteSenhaSignup = false;
+    }
+    else {
+        labelInputRepeteSenhaSignup.style.color = '#592c12';
+        labelInputRepeteSenhaSignup.innerHTML = "repete senha:";
+        validRepeteSenhaSignup = true;
+    }
+}
+;
+function verificaCampos() {
+    let retorno = true;
+    let usuarioExistente = (JSON.parse(localStorage.getItem("usuarios") || "[]")
+        .some((usuario) => usuario.email === JSON.stringify(inputEmailSignup.value)));
+    if (inputEmailSignup.value === "" || inputSenhaSignup.value === "" || inputRepeteSenhaSignup.value === "") {
+        alert("Você deve preencher todos os campos!");
+        retorno = false;
+    }
+    else if (!validEmailSignup || !validSenhaSignup || !validRepeteSenhaSignup) {
+        alert("Você deve preencher todos os campos!");
+        retorno = false;
+    }
+    else if (usuarioExistente) {
+        alert("E-mail já cadastrado!");
+        retorno = false;
+    }
+    else {
+        alert("Cadastro efetuado com sucesso!");
+    }
+    return retorno;
+}
+;
+function criarRegistroUsuario() {
+    let dadosStorage = JSON.parse(window.localStorage.getItem("usuarios") || "[]");
+    let email = inputEmailSignup.value;
+    let senha = inputSenhaSignup.value;
+    let user = {
+        email,
+        senha,
+        recados: []
+    };
+    dadosStorage.push(user);
+    window.localStorage.setItem("usuarios", JSON.stringify(dadosStorage));
+    // redirecionaPaginaInicial();
+}
+function limparCampos() {
+    inputEmailSignup.value = "";
+    inputSenhaSignup.value = "";
+    inputRepeteSenhaSignup.value = "";
+}
+;
+// function redirecionaPaginaInicial() {
+//     window.location.href = "./index.html";
+// }
---
+++ src/index.ts
@@ -0,0 +1,244 @@
+//LOGIN
+let formLogin = document.querySelector("#form-login") as HTMLFormElement;
+
+let inputEmailLogin = document.querySelector("#input-email") as HTMLInputElement;
+let inputSenhaLogin = document.querySelector("#input-senha") as HTMLInputElement;
+let btnCriar = document.querySelector("#btn-criar") as HTMLButtonElement;
+let btnVoltarLogin = document.querySelector("#btn-voltar-login") as HTMLButtonElement;
+let divPrincipalLogin = document.querySelector("#div-principal-login") as HTMLDivElement;
+let divPrincipalSignup = document.querySelector("#div-principal-signup") as HTMLDivElement;
+
+let usuarios : Usuario[] = JSON.parse(localStorage.getItem("usuarios") || "[]");
+
+interface Usuario {
+    email: string,
+    senha: string,
+    recados: any []
+}
+
+// CADASTRO
+let formSignup = document.querySelector("#form-signup") as HTMLFormElement;
+
+let labelInputEmailSignup = document.querySelector("#label-input-email") as HTMLLabelElement;
+let inputEmailSignup = document.querySelector("#input-signup-email") as HTMLInputElement;
+let validEmailSignup : boolean = false;
+
+let labelInputSenhaSignup = document.querySelector("#label-input-senha") as HTMLLabelElement;
+let inputSenhaSignup = document.querySelector("#input-signup-senha")  as HTMLInputElement;
+let validSenhaSignup : boolean = false;
+
+let labelInputRepeteSenhaSignup = document.querySelector("#label-input-repete-senha") as HTMLLabelElement;
+let inputRepeteSenhaSignup = document.querySelector("#input-signup-repete-senha") as HTMLInputElement;
+let validRepeteSenhaSignup : boolean = false;
+
+let regSenha : RegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+
+
+
+// ALERTAS
+let espacoAlertaIndex = document.getElementById('espaco-alerta-index') as HTMLDivElement;
+let corpoAlertaIndex: HTMLDivElement = document.createElement('div');
+
+function mostrarAlertaIndex(mensagem: string, tipo: string) {
+    corpoAlertaIndex.innerHTML = `
+                        <div class="alert alert-${tipo} alert-dismissible fade show" role="alert">
+                                <div>${mensagem}</div>
+                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                        `;                                                
+    espacoAlertaIndex.appendChild(corpoAlertaIndex);
+    setTimeout(() => {corpoAlertaIndex.innerHTML = '';}, 3000);
+};
+
+// EVENTOS
+formLogin.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (verificaCamposLogin()) {
+        if (validaUsuarioLogin() && validaSenhaLogin()) {
+            salvarNoLocalStorage();
+            redirecionaParaPaginaPainel();
+        }
+    }
+});
+
+btnCriar.addEventListener("click", () => {
+    formLoginHide();
+    formSignupShow();
+});
+
+btnVoltarLogin.addEventListener("click", () => {
+    formSignupHide();
+    formLoginShow();
+});
+
+inputEmailSignup.addEventListener("keyup", verificaEmailSignup);
+inputSenhaSignup.addEventListener("keyup", verificaSenhaSignup);
+inputRepeteSenhaSignup.addEventListener("keyup", verificaRepeteSenhaSignup);
+
+formSignup.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if( verificaCampos() ) {
+        criarRegistroUsuario();
+        limparCampos();
+    }
+});
+
+// FUNÇÕES
+function verificaCamposLogin(){
+
+    let retorno = true;
+
+    if(inputEmailLogin.value === "" || inputSenhaLogin.value ===""){
+        mostrarAlertaIndex("Você deve preencher todos os campos!", "warning");
+        retorno = false;
+    }
+    return retorno;
+}
+
+function validaUsuarioLogin(){
+
+    let usuarioExistente : boolean = usuarios.some((usuario) => usuario.email === inputEmailLogin.value);
+    
+    if (!usuarioExistente) {
+        mostrarAlertaIndex("Usuário não cadastrado! Primeiro crie uma conta.", "warning");
+    }
+    return usuarioExistente;
+}
+
+
+function validaSenhaLogin(){
+
+    let usuario = usuarios.find((usuario) => usuario.email === inputEmailLogin.value);
+    let senhaCorreta  = usuario ? usuario.senha === inputSenhaLogin.value : false;
+
+    if (!senhaCorreta) {
+        mostrarAlertaIndex("Senha incorreta, tente novamente.", "danger");
+    }
+    return senhaCorreta;
+}
+
+function redirecionaParaPaginaPainel() {
+    alert("Você logou!");
+    window.location.href = "../public/painel.html";
+}
+
+function salvarNoLocalStorage() {
+    localStorage.setItem("usuario_logado", inputEmailLogin.value);
+}
+
+function formLoginHide(){
+    divPrincipalLogin.classList.add("d-none");
+}
+
+function formSignupShow(){
+    divPrincipalSignup.classList.remove("d-none");
+}
+
+function formSignupHide(){
+    divPrincipalSignup.classList.add("d-none");
+}
+
+function formLoginShow(){
+    divPrincipalLogin.classList.remove("d-none");
+}
+
+function verificaEmailSignup(){ 
+
+    if(inputEmailSignup.value.length < 10){
+        labelInputEmailSignup.style.color = 'red';
+        labelInputEmailSignup.innerHTML = "e-mail: * insira no mínimo 10 caracteres";
+        validEmailSignup = false;
+    } else{   
+        labelInputEmailSignup.style.color = '#592c12';    
+        labelInputEmailSignup.innerHTML = "e-mail:";
+        validEmailSignup = true;
+    }    
+};
+
+function verificaSenhaSignup(){  
+    let senhaValida = inputSenhaSignup.value.match(regSenha);   
+
+    if(inputSenhaSignup.value.length < 6){
+        labelInputSenhaSignup.style.color = 'red';
+        labelInputSenhaSignup.innerHTML = "senha: * insira no mínimo 6 caracteres";
+        validSenhaSignup = false;
+
+    } else if(senhaValida === null){
+        labelInputSenhaSignup.innerHTML = "senha: * senha deve conter uma letra maiúscula e um caracter especial";
+        labelInputSenhaSignup.style.color = 'red';
+        validSenhaSignup = false;
+
+    } else {      
+        labelInputSenhaSignup.style.color = '#592c12'; 
+        labelInputSenhaSignup.innerHTML = "senha:";
+        validSenhaSignup = true;
+    }
+};
+
+function verificaRepeteSenhaSignup(){   
+
+    if(inputRepeteSenhaSignup.value !== inputSenhaSignup.value){
+        labelInputRepeteSenhaSignup.style.color = 'red';
+        labelInputRepeteSenhaSignup.innerHTML = "repete senha: * a senha digitada não corresponde";
+        validRepeteSenhaSignup = false;
+    } else {      
+        labelInputRepeteSenhaSignup.style.color = '#592c12';
+        labelInputRepeteSenhaSignup.innerHTML = "repete senha:";
+        validRepeteSenhaSignup = true;
+    }
+};
+
+function verificaCampos(){
+
+    let retorno = true;
+
+    let usuarioExistente: boolean = (JSON.parse(localStorage.getItem("usuarios") || "[]")
+        .some((usuario: Usuario) => usuario.email === JSON.stringify(inputEmailSignup.value)));
+
+    
+    if(inputEmailSignup.value === "" || inputSenhaSignup.value ==="" || inputRepeteSenhaSignup.value ===""){
+        alert("Você deve preencher todos os campos!");
+        retorno = false;
+
+    } else if(!validEmailSignup || !validSenhaSignup || !validRepeteSenhaSignup){
+        alert("Você deve preencher todos os campos!");
+        retorno = false;
+
+    } else if(usuarioExistente){
+        alert("E-mail já cadastrado!");
+        retorno = false;
+
+    } else {
+        alert("Cadastro efetuado com sucesso!");
+
+    }
+
+    return retorno;
+};
+
+function criarRegistroUsuario(){
+    let dadosStorage : Usuario[] = JSON.parse(window.localStorage.getItem("usuarios") || "[]");
+    let email = inputEmailSignup.value;
+    let senha = inputSenhaSignup.value;
+
+    let user: Usuario = {
+        email,
+        senha,
+        recados: []
+    }
+
+    dadosStorage.push(user);
+    
+    window.localStorage.setItem("usuarios", JSON.stringify(dadosStorage));
+    // redirecionaPaginaInicial();
+}
+
+function limparCampos(){
+    inputEmailSignup.value = "";
+    inputSenhaSignup.value = "";
+    inputRepeteSenhaSignup.value = "";
+};
+
+// function redirecionaPaginaInicial() {
+//     window.location.href = "./index.html";
+// }
